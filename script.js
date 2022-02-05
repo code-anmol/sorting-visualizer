21aa71458bc64bb07c9210c518ed2d7741ef791b
Anmol <anmol.coding@gmail.com>
2022-02-05 14:00:10 -0500

    fixed reload button and added list items

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ let list = document.getElementById("list");
 let length = 20;
 
 let nums = generateRandomNumbers(length);
-console.log(nums);
+
+let listItem = document.createElement("li");
+let listItem2 = document.createElement("li");
+listItem.innerHTML = "a";
+listItem2.innerHTML = "a";
+listItem.style.height = nums[0]+"px";
+listItem2.style.height = nums[1]+"px";
+
+list.appendChild(listItem);
+list.appendChild(listItem2);
+
+
+
 
 
 
@@ -14,4 +26,8 @@ function generateRandomNumbers(length){
     }
 
     return nums;
+}
+
+function reloadPage(){
+    location.reload();
 }
